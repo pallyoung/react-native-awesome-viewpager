edb94eb25b4dba37fe472f8a2a9971a4a1443e25
Spencer <pallyoung@163.com>
2018-06-29 19:12:32 +0800

    view pager setpage触发事件

--- src/ViewPager.android.js
+++ src/ViewPager.android.js
@@ -1,3 +1,24 @@
 'use strict'
 import {ViewPagerAndroid} from 'react-native';
-export default ViewPagerAndroid;
+
+
+class ViewPager extends ViewPagerAndroid{
+    static propTypes = {
+        ...ViewPagerAndroid.propTypes,
+    }
+    constructor(...props){
+        super(...props);
+
+        this._setPageWithoutAnimation = this.setPageWithoutAnimation;
+        this._setPage = this.setPage;
+        this.setPageWithoutAnimation = function(position){
+            this._setPageWithoutAnimation(position);
+            this.props.onPageSelected && this.props.onPageSelected({ nativeEvent: { position } });
+        }
+        this.setPage = function(position){
+            this._setPage(position);
+            this.props.onPageSelected && this.props.onPageSelected({ nativeEvent: { position} });
+        }
+    }
+}
+export default ViewPager;
